95583ae766d35e98ba7cc7177a655f6d3745776c
Franlex Ramirez <franlex.ramirez@gmail.com>
2024-12-01 15:29:17 -0400

    aprendiendo ES6

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 let course = ["HTML","JS","CSS"];
 console.log(course[2]);
+
+
+
+//aprendiendo ES6
+//Funciones ES6
+const add = (x,y) => {
+    let sum = x + y;
+    console.log(sum);
+}
+
+add(35,7);
