de687b88af31157ffdd9c79d7e21846cb779fa3c
Franlex Ramirez <franlex.ramirez@gmail.com>
2024-12-02 18:50:16 -0400

    Promises in ES6

--- app.js
+++ app.js
@@ -11,3 +11,31 @@ const add = (x,y) => {
 }
 
 add(35,7);
+
+
+//ES6 Promises
+function asyncFunc(work) {
+    return new Promise(function(resolve, reject){
+        if(work === '')
+            reject(Error("Nothing"));
+        setTimeout(function(){
+            resolve(work);
+        }, 1000);
+    });
+}
+
+asyncFunc("Work 1") //Task 1
+.then(function(result){
+    console.log(result);
+    return asyncFunc("Work 2"); //Task 2
+}, function(error){
+    console.log(error);
+})
+.then(function(result){
+    console.log(result);
+}, function(error) {
+    console.log(error);
+});
+console.log("End");
+//Work 1
+//Work 2
